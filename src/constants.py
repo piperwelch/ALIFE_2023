d7ef412e3c87994fc06693d5591783a6354558e8
Piper Welch <63561939+piperwelch@users.noreply.github.com>
2024-07-22 12:45:59 -0400

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,6 @@ STARTING_AREA_DIM = BOT_DIAM*4+8 #(7 bots, 4 starting quadrants, plus 8 in case 
 # For fitness function calculations
 LATTICE_DIM = 0.01 # 1 voxel is LATTICE_DIM meters
 BOT_LENGTH = 8*LATTICE_DIM # diameter in voxels * lattice_dim
-BOUNDARY_LENGTH_X = 320*LATTICE_DIM
-BOUNDARY_LENGTH_Y = 320*LATTICE_DIM
+BOUNDARY_LENGTH_X = 320*LATTICE_DIM #change this based on environment size 
+BOUNDARY_LENGTH_Y = 320*LATTICE_DIM #change this based on environment size 
 MAX_LEVEL = 13
